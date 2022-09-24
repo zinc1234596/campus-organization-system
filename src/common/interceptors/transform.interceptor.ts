879db0ff9a133503af7b4def92af4d679f987828
ryan.mac <1695073263@qq.com>
2022-09-24 22:55:30 +0800

    系统基础配置-3-(全局返回参数)

---
+++ src/common/interceptors/transform.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+// 响应统一interface
+interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+// 转换拦截器类
+// implements:https://blog.csdn.net/weixin_49343253/article/details/123425046
+export class TransformInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext, // 执行上下午
+    next: CallHandler, // next处理
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => ({
+        data, // 数据
+        status: 0, // 接口状态值
+        extra: {}, // 拓展信息
+        message: 'success', // 异常信息
+        success: true, // 接口业务返回状态
+      })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('ucs'); // 修改全局路由前缀
-  await app.listen(2022); // 修改全局端口
+  // 修改全局路由前缀
+  app.setGlobalPrefix('ucs');
+  // 统一响应体格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 修改全局端口
+  await app.listen(2022);
 }
 bootstrap();
