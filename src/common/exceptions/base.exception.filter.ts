86aeaa8470323a0f77d386fb7cc5f6ad7786d9b7
ryan.mac <1695073263@qq.com>
2022-09-24 23:39:27 +0800

    系统基础配置-4-(全局异常拦截-异常过滤器)

---
+++ src/common/exceptions/base.exception.filter.ts
@@ -0,0 +1,26 @@
+// 处理统一异常
+import { Request, Response } from 'express';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+  ServiceUnavailableException,
+} from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    // 非 HTTP 标准异常处理。
+    response.status(HttpStatus.SERVICE_UNAVAILABLE).json({
+      statusCode: HttpStatus.SERVICE_UNAVAILABLE,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: new ServiceUnavailableException().getResponse(),
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 修改全局路由前缀
   app.setGlobalPrefix('ucs');
   // 统一响应体格式
   app.useGlobalInterceptors(new TransformInterceptor());
+  // 全局异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
   // 修改全局端口
   await app.listen(2022);
 }
